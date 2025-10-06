2dd85ab6037c30f3cca67e965bba784fdf9d0e76
Codeacula <codeacula@codeacula.com>
2025-10-06 11:18:24 -0500

    Adding test projects

---
+++ tests/Orchid.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Orchid.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Orchid.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Orchid.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
